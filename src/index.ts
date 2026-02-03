c2910402f1466cfab95451d3cc5ec3fb6581787a
xup60521 <84887662+xup60521@users.noreply.github.com>
2026-02-03 14:18:42 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { serve } from "bun";
+import index from "./index.html";
+import app from "api";
+
+const server = serve({
+    routes: {
+        // Serve index.html for all unmatched routes.
+        "/*": index,
+
+        "/api/*": app.fetch,
+    },
+
+    development: process.env.NODE_ENV !== "production" && {
+        // Enable browser hot reloading in development
+        hmr: true,
+
+        // Echo console logs from the browser to the server
+        console: true,
+    },
+});
+
+console.log(`🚀 Server running at ${server.url}`);
